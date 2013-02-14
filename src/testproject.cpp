4c050e9de3d73b082c9947061c188573b79f8d1c
pieter <ppottas@gmail.com>
2013-02-14 21:06:59 +0200

    added hi there

---
+++ src/testproject.cpp
@@ -0,0 +1,14 @@
+//============================================================================
+// Name        : testproject.cpp
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello World!!!" << endl; // prints Hello World!!!
+
+	cout << "hi there.";
+	return 0;
+}
